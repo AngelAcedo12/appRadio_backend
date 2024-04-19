2a6d4c64c8a42b64d921e196770af450a563b4fc
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-19 18:38:09 +0200

    s

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.listen(3004, () => {
+    console.log('Server is running on http://localhost:3004');
+    }  );
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
