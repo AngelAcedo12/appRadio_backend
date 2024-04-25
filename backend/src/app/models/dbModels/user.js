7b8839d0a4338b297eea3582906833e3bf517c82
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-25 17:25:19 +0200

    s

--- backend/src/app/models/dbModels/user.js
+++ backend/src/app/models/dbModels/user.js
@@ -31,4 +31,4 @@ const userSchema = new mongoose.Schema({
     ]
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('users', userSchema) || mongoose.models.users;
--- backend/src/app/utils/encrypterToken.js
+++ backend/src/app/utils/encrypterToken.js
@@ -13,7 +13,7 @@ const tokenDecrypter = async(token) => {
     try {
         
      const tokenDecode= await jwt.decode(token, process.env.SECRET);
-     console.log(tokenDecode, "tokenDecode")
+     
      return tokenDecode;
     } catch (error) {
         return false;
