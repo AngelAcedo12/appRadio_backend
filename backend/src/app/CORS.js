c56c86e3bf6608eea436789ccfae97c5fc3ba556
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-25 09:47:20 +0200

    s

--- backend/src/app/CORS.js
+++ backend/src/app/CORS.js
@@ -4,5 +4,4 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   
-    
-  }
+}
