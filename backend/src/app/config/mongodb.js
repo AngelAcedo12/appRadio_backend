e1cfc9b1a462618c00fd6ba5023b27bcd53d8d60
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-26 12:22:27 +0200

    update conection

--- backend/src/app/config/mongodb.js
+++ backend/src/app/config/mongodb.js
@@ -1,13 +1,7 @@
 import mongose from 'mongoose';
 
-let DB_URL = ""
-if(process.env.NODE_ENV === 'development'){
-    DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/pruebasAppRadio';
-    
-}else{
-     DB_URL = process.env.DB_URL_PRO
-}
 
+const DB_URL = process.env.NODE_ENV === 'production' ? process.env.DB_URL_PROD : process.env.DB_URL;
 
 const dbConnect = async () => {
     try {
