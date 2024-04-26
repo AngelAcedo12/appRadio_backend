d97b8849517c4552f97555d95135cbeb5f47a2c3
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-04-26 12:18:16 +0200

    Update mongodb.js

--- backend/src/app/config/mongodb.js
+++ backend/src/app/config/mongodb.js
@@ -1,7 +1,7 @@
 import mongose from 'mongoose';
 
 let DB_URL = ""
-if(process.env.NODE_ENV !== 'production'){
+if(process.env.NODE_ENV === 'development'){
     DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/pruebasAppRadio';
     
 }else{
