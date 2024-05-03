857b47fc83e7ba5b3a0dcb71e4a36bee301e7b62
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-03 14:22:42 +0200

    Add POST method to recomendations route

--- backend/src/app/api/recomendations/route.js
+++ backend/src/app/api/recomendations/route.js
@@ -1,20 +1,36 @@
+import { tokenDecrypter } from "@/app/utils/encrypterToken";
 import { dbConnect } from "../../config/mongodb";
 import userSchema from "../../models/dbModels/user";
 
 await dbConnect();
-export async function GET(){
+export async function POST(request){
+    console.log("RECOMENDATIONS")
+    const {token} = await request.json();
+
+    const userToken = await tokenDecrypter(token).then((res) => {
+        return res.user;
+    });
 
     const recomendations =  await userSchema.aggregate([ { $sample: { size: 10 } } ]).then((res) => {	
 
         return res
        
     }
+
+
+
     ).catch((error) => {
       console.log(error)
       return {}
     }
     )
-    const response = recomendations.map((item) => {
+    const response = recomendations.filter((item) => {
+        if(item.email !== userToken.email){
+            return item
+        }
+        
+    }).map((item) => {
+        
         return {
             name: item.name,
             email: item.email,
