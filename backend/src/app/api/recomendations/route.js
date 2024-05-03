44583b346211cf146400a3f94f5d8cdbd7d947c3
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-03 13:46:44 +0200

    Refactor user route and add recomendations endpoint

--- backend/src/app/api/recomendations/route.js
+++ backend/src/app/api/recomendations/route.js
@@ -1,9 +1,27 @@
 import { dbConnect } from "../../config/mongodb";
-
+import userSchema from "../../models/dbModels/user";
 
 await dbConnect();
-export function GET(){
+export async function GET(){
 
+    const recomendations =  await userSchema.aggregate([ { $sample: { size: 10 } } ]).then((res) => {	
 
+        return res
+       
+    }
+    ).catch((error) => {
+      console.log(error)
+      return {}
+    }
+    )
+    const response = recomendations.map((item) => {
+        return {
+            name: item.name,
+            email: item.email,
+            imgProfile: item.imgProfile
 
+        }
+    }
+    )
+    return new Response(JSON.stringify(response));
 }
