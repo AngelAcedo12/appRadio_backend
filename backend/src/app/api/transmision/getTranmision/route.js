142e26f8c142dd94e74287ba9e78658edb8dad01
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-10 09:35:22 +0200

    chore: Update getTranmision route to handle empty transmisions

--- backend/src/app/api/transmision/getTranmision/route.js
+++ backend/src/app/api/transmision/getTranmision/route.js
@@ -5,11 +5,18 @@ await dbConnect();
 export async function GET(){
     
     const transmisions = await transmisionSchema.find().then((res) => {
+
         return res
     }).catch((err) => {
         return err  
     })
+
+    if(transmisions===null || transmisions===undefined){
+        return new Response(JSON.stringify({message:"No se encontraron transmisiones"}), {status: 404})
+    }
     
-    return new Response(JSON.stringify(transmisions), {status: 200})
+ 
+
+    return new Response(JSON.stringify({data:transmisions}), {status: 200})
 
 }
