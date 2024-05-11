cc91d03930047a8a230a98647ebba6734ed29827
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-11 10:15:04 +0200

    asd

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const next = require("next");
 const { Server } = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = dev=="production" ?  "appradio-backend.onrender.com" : "localhost";
+const hostname = "localhost";
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 
